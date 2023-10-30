bf93fe9706825e99c9b32d74eb4f15e848f2ef3f
shiva Concept Solution <50239824+shivaconceptsolution@users.noreply.github.com>
2023-10-30 11:58:04 +0530

    Add files via upload

--- parentsapp/urls.py
+++ parentsapp/urls.py
@@ -3,8 +3,13 @@ from . import views
 urlpatterns=[
    path('',views.home,name='home'),
    path('aboutus',views.aboutus,name='aboutus'),
+   path('editstudent',views.editstudent,name='editstudent'),
+   path('deletestudent',views.deletestudent,name='deletestudent'),
    path('services',views.services,name='services'),
    path('gallery',views.gallery,name='gallery'),
-   path('contactus',views.contactus,name='contactus') 
+   path('contactus',views.contactus,name='contactus'),
+   path('signup',views.signup,name='signup'),
+   path('signin',views.signin,name='signin'),
+   path('dashboard',views.dashboard,name='dashboard')
 
 ]
--- parentsapp/views.py
+++ parentsapp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import Contact
+from .models import Contact,Register
 
 def home(request):
     if request.method=="POST":
@@ -11,9 +11,29 @@ def home(request):
     return render(request,"parentsapp/index.html")
 
 def aboutus(request):
-    data = Contact.objects.all()
-    return render(request,"parentsapp/about.html",{'key':data})
+    
+    return render(request,"parentsapp/about.html")
  
+
+def editstudent(request):
+    data = Contact.objects.get(pk=request.GET["id"])
+    if request.method=="POST":
+        data.name = request.POST.get("txtname")
+        data.phoneno=request.POST["txtphone"]
+        data.email = request.POST["txtemail"]
+        data.message = request.POST["txtmessage"]
+        data.save()
+        return redirect('/parentsapp/aboutus')
+
+    return render(request,"parentsapp/editstudent.html",{'key':data})
+
+
+def deletestudent(request):
+    data = Contact.objects.get(pk=request.GET["id"])
+    if request.method=="POST":
+        data.delete()
+        return redirect('/parentsapp/aboutus')
+    return render(request,"parentsapp/deletestudent.html",{'key':data})
 
 def services(request):
     return render(request,"parentsapp/services.html")
@@ -25,3 +45,23 @@ def gallery(request):
 
 def contactus(request):
     return render(request,"parentsapp/contactus.html")
+
+def signup(request):
+    if request.method=="POST":
+        obj = Register(emailid=request.POST["txtemail"],password=request.POST["txtpass"],mobile=request.POST["txtmobile"],fullname=request.POST["txtname"])
+        obj.save()
+        return redirect('/parentsapp/signin')
+
+    return render(request,"parentsapp/signup.html")
+
+def signin(request):
+    if request.method=="POST":
+        obj = Register.objects.filter(emailid=request.POST["txtemail"],password=request.POST["txtpass"])
+        if(obj.count()>0):
+            return redirect('/parentsapp/dashboard')
+        else:
+            return render(request,"parentsapp/signin.html",{"key":"login fail"})
+    return render(request,"parentsapp/signin.html")
+def dashboard(request):
+    data = Contact.objects.all()
+    return render(request,"parentsapp/dashboard.html",{'key':data})
